062d7bc6c45a42ba65c5e94c30f8ad23e3190837
faii55011212090 <pacharin.fai@gmail.com>
2015-01-16 16:51:28 +0700

    array1

--- Array.playground/section-1.swift
+++ Array.playground/section-1.swift
@@ -17,3 +17,16 @@ var firstItem = shoppingList[0]
 shoppingList[0] = "Six eggs"
 //shoppingList[0...3] = ["Bananas" , "Apples"]
 shoppingList
+
+
+var array1 = ["abc","bcd","cdf"]
+for (var i = 0; i < array1.count; i++) {
+    println(array1[1])
+}
+
+
+var array2 = ["a","b","c"]
+var str = ""
+for str in array2 {
+    println(str)
+}

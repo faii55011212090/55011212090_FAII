172fd3c9390bdda94ddbb2bcfe6e934ecfa04570
faii55011212090 <pacharin.fai@gmail.com>
2015-01-16 15:51:17 +0700

    week1
    Flow

---
+++ TestWeek1.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let tutorialTeam = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+let priceIntInferred = -19
+let pricrIntExplicit:Int = -19
+
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSeleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+
+
